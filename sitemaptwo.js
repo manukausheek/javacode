292456d2a8832351711271b0d7112172b6d05db0
manukausheek <132734934+manukausheek@users.noreply.github.com>
2023-05-06 11:32:05 +0530

    Add files via upload

---
+++ sitemaptwo.js
@@ -0,0 +1,35 @@
+var request = new XMLHttpRequest();
+		request.open('GET', '/feeds/posts/default/-/Job?max-results=21&alt=json', true);
+
+		request.onload = function() {
+			if (request.status >= 200 && request.status < 400) {
+				var data = JSON.parse(request.responseText);
+				var posts = data.feed.entry;
+
+				var sitemaptwo = document.getElementById('sitemaptwo');
+				
+				for (var i = 0; i < posts.length; i++) {
+					var title = posts[i].title.$t;
+					var link = posts[i].link[4].href;
+					var published = new Date(posts[i].published.$t).toLocaleDateString('en-GB');
+
+					var listItem = document.createElement('li');
+					listItem.innerHTML = '<a href="' + link + '">' + title + '</a> (' + published + ')';
+
+					if (i < 5) {
+						listItem.classList.add('new');
+						listItem.innerHTML = 'New! ' + listItem.innerHTML;
+					}
+
+					sitemaptwo.appendChild(listItem);
+				}
+			} else {
+				console.log('Error');
+			}
+		};
+
+		request.onerror = function() {
+			console.log('Error');
+		};
+
+		request.send();
